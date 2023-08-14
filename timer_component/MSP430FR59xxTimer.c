d0bd1f6394ed295530aed80d79433331c439ee7e
joaogpd <joaogdrumond@aluno.puc-rio.br>
2023-08-14 12:01:41 -0300

    corrected timer

--- timer_component/MSP430FR59xxTimer.c
+++ timer_component/MSP430FR59xxTimer.c
@@ -14,8 +14,7 @@ extern volatile uint16_t _vtTasksTimer[MAX_VT_TASKS]; /**< Array of timer counte
  */
 void startClockTimer() {
   TA0CCTL0 = CCIE; // TACCR0 interrupt enabled
-  TA0CCR0 = 32768 * 0.016; // preload timer, 32768Hz on ACLK, 16 ms timer
-  // clock frequency is not 32768Hz, fix this
+  TA0CCR0 = FREQUENCY * 0.016; // preload timer, ACLK frequency, 16 ms timer
   TA0R = 0; // ensure timer starts counting from zero
   TA0CTL = TASSEL__ACLK | MC__UP; // ACLK, up mode
 }
@@ -35,7 +34,6 @@ void __attribute__  ((interrupt(TIMER0_A0_VECTOR))) Timer0_A0_ISR(void)
 #error Compiler not supported!
 #endif
 {
-  // TA0R = 0 // reset timer value to count up again // not needed anymore in up mode (in theory)
   for (uint8_t i = 0; i < MAX_VT_TASKS; i++){
     if (_vtTasksTimer[i] > 0) {
       _vtTasksTimer[i]--;
